792025ff8b24c6c7da3225e3319673a2811c8895
Guilherme Reichert <guireichert.tr@gmail.com>
2025-03-19 14:11:33 -0300

    Creating User Creation Endpoint

---
+++ MyRecipeApp/src/Backend/MyRecipeApp.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeApp.Communications;
+using MyRecipeApp.Communications.Requests;
+using MyRecipeApp.Communications.Responses;
+
+namespace MyRecipeApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+
+        }
+    }
+}
